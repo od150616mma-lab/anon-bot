5496040914a9ff82d04a69fc7c257a58ca89cba3
od150616mma-lab <od150616mma@gmail.com>
2026-07-05 00:56:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,42 @@
+import os
 import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
 
-TOKEN = "8683974265:AAEDO5FnvCLIEMmgVeyT2aRv6jXoT-9DaTE"
+TOKEN = os.getenv(8683974265:AAEDO5FnvCLIEMmgVeyT2aRv6jXoT-9DaTE)
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+
+# Укажи здесь свой Telegram ID
+OWNER_ID = 123456789
+
 
 @dp.message(Command("start"))
 async def start(message: Message):
     await message.answer(
         "👋 Привет!\n\n"
-        "Напиши сюда сообщение, и оно будет отправлено владельцу бота анонимно."
+        "Напиши сюда сообщение, и оно будет отправлено владельцу анонимно."
     )
+
 
 @dp.message()
 async def anonymous(message: Message):
-    owner_id = 123456789  # Потом заменим на твой Telegram ID
-
     await bot.send_message(
-        owner_id,
-        f"📩 Новое анонимное сообщение:\n\n{message.text}"
+        OWNER_ID,
+        f"📩 Новое анонимное сообщение\n\n"
+        f"👤 От: {message.from_user.full_name}\n"
+        f"🆔 ID: {message.from_user.id}\n\n"
+        f"💬 {message.text}"
     )
 
     await message.answer("✅ Сообщение отправлено анонимно!")
 
+
 async def main():
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
