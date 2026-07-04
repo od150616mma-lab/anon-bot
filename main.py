4d55c05b546e7d68c22373dfdc6043a2541a6475
od150616mma-lab <od150616mma@gmail.com>
2026-07-05 01:16:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-# Укажи здесь свой Telegram ID
+# Твой Telegram ID
 OWNER_ID = 8760172354
 
 
@@ -19,6 +19,38 @@ async def start(message: Message):
         "👋 Привет!\n\n"
         "Напиши сюда сообщение, и оно будет отправлено владельцу анонимно."
     )
+
+
+@dp.message(Command("r"))
+async def reply_command(message: Message):
+    if message.from_user.id != OWNER_ID:
+        return
+
+    parts = message.text.split(maxsplit=2)
+
+    if len(parts) < 3:
+        await message.answer(
+            "Использование:\n"
+            "/r ID сообщение"
+        )
+        return
+
+    try:
+        user_id = int(parts[1])
+    except ValueError:
+        await message.answer("❌ ID должен быть числом.")
+        return
+
+    text = parts[2]
+
+    try:
+        await bot.send_message(
+            chat_id=user_id,
+            text=f"💬 Анонимный ответ:\n\n{text}"
+        )
+        await message.answer("✅ Ответ отправлен!")
+    except Exception as e:
+        await message.answer(f"❌ Ошибка: {e}")
 
 
 @dp.message()
